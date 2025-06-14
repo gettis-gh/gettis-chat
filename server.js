9e2627f207d68fe7d484cf66b41b04c271451792
frgle <frglemelito@gmail.com>
2025-06-13 21:21:23 -0400

    minor change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import sequelize from './src/database/sequelize.js';
 
 (async () => {
   try {
-      await sequelize.sync({ force: true });
+      await sequelize.sync();
       console.log("✅ Database synced");
   } catch (error) {
       console.error("❌ DB sync error:", error.message);
